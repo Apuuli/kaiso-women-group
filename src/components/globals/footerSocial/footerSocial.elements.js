df6683c034d1316d18298c9434028e1c50ab2908
Apuuli John <apuulijohn@gmail.com>
2021-04-18 22:29:53 +0300

    footer ls

---
+++ src/components/globals/footerSocial/footerSocial.elements.js
@@ -0,0 +1,17 @@
+import styled from "styled-components"
+
+export const FooterSocialContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+export const FooterSocialItem = styled.a`
+  display: inline-flex;
+  align-items: center;
+  text-decoration: none;
+  font-weight: 600;
+  margin: 0.25rem 1rem;
+  text-align: center;
+  span {
+    margin-left: 2rem;
+  }
+`
